3d4cbe3058730ac7228a0546cd042c0eca8aec22
Donghwan Oh <imodh97@gmail.com>
2024-05-14 10:56:30 +0900

    Server fix

--- controllers/programController.js
+++ controllers/programController.js
@@ -1,8 +1,21 @@
 import supabase from "../db.js";
 
 export const getPrograms = async (req, res) => {
-  const { data, error } = await supabase.from("BUSINESS_SAMPLE").select("*");
-  if (error) console.log(error.message);
+  const {
+    query: { keyword },
+  } = req;
+  if (keyword) {
+    const { data, error } = await supabase
+      .from("PROGRAM")
+      .select("*")
+      .eq("program_name", "test2");
+    if (error) console.log(error.message);
 
-  res.status(200).json({ ok: true, data });
+    return res.status(200).json({ ok: true, data });
+  } else {
+    const { data, error } = await supabase.from("PROGRAM").select("*");
+    if (error) console.log(error.message);
+
+    return res.status(200).json({ ok: true, data });
+  }
 };
--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 export default supabase;
